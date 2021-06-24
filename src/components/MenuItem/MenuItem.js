6118513de87c314541c5c2901123068df1d04db3
Wesley <wesleyadwebster@gmail.com>
2021-06-23 23:44:03 -0500

    initial clothing categories styled, font added

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -2,14 +2,14 @@ import React from 'react';
 import '../../App.scss';
 import classes from './MenuItem.module.scss';
 
-const {menuItem, titleContainer, title, subtitle} = classes;
+const { menuItem, titleContainer, title, subtitle, backgroundImage } = classes;
 
 const MenuItem = (props) => {
     return (
-        <div className={`${menuItem} ${props.imageClass}`}>
+        <div className={`${menuItem} ${props.imageClass} ${backgroundImage}`}>
             <div className={titleContainer}>
-            <h1 className={title}>{props.title}</h1>
-            <span className={subtitle}>{props.cta}</span>
+                <h1 className={title}>{props.title}</h1>
+                <span className={subtitle}>{props.cta}</span>
             </div>
         </div>
     );
