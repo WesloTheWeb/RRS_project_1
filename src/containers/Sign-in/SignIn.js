0a6883e7ae26caad17637683cda841a88a3b46ac
Wesley <wesleyadwebster@gmail.com>
2021-06-30 19:01:19 -0500

    Sign in logic for email and password onChange created. Made a state object containing the two values

--- src/containers/Sign-in/SignIn.js
+++ src/containers/Sign-in/SignIn.js
@@ -4,29 +4,34 @@ import classes from '../../components/LoginPage/LoginPage.module.scss';
 const { terminalUIBlock, loginCommon } = classes;
 
 const SignIn = (props) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    })
+
     const handleSubmit = (e) => {
-        e.preventDefault(); // Prevents default from happening.
-        setEmail(email = '');
-        setPassword(password = '');
+        e.preventDefault(); 
+        console.log(form.email);
+        console.log(form.password);
+        setForm({
+            email: '',
+            password: '',
+          });
     }
 
-    // const handleChange = (e) => {
-    //     const { value, name } = e.target;
-    //     setEmail({name: value});
-    //     setPassword({password: value});
-    // }
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+      }
 
     return (
         <div className={terminalUIBlock}>
             <h2>I already have an account</h2>
             <span>Sign in with your email and password</span>
-            <form className={loginCommon}>
+            <form onSubmit={handleSubmit} className={loginCommon}>
                 <label>Email</label>
-                <input name="email" value={email} type="email" required />
+                <input name="email" value={form.email} type="email" onChange={handleChange} required />
                 <label>Password</label>
-                <input name="password" value={password} type="password" required />
+                <input name="password" value={form.password} onChange={handleChange} type="password" required />
                 <input type="submit" value="submit"></input>
             </form>
         </div>
